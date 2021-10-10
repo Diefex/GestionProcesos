5db1dfab4e0e0b4a5f6ab6fff2110e981d871033
Diefex <diefex57@gmail.com>
2021-10-09 19:15:29 -0500

    Lista Predeterminada

--- Sistema.py
+++ Sistema.py
@@ -10,12 +10,6 @@ class Sistema:
         self.procesos = []
 
         self.lista_procesos = []
-        # self.lista_procesos.append(Proceso(0, 6, [[3,2]]))
-        # self.lista_procesos.append(Proceso(1, 8, [[1,3]]))
-        # self.lista_procesos.append(Proceso(2, 7, [[5,1]]))
-        # self.lista_procesos.append(Proceso(4, 3, [[0,0]]))
-        # self.lista_procesos.append(Proceso(6, 9, [[2,4]]))
-        # self.lista_procesos.append(Proceso(6, 2, [[0,0]]))
 
         self.display = None
         self.planificador = FCFS()
@@ -44,7 +38,7 @@ class Sistema:
             self.procesos[i].atender()
         # pintar procesos
         self.display.pintar_procesos(self.procesos)
-        for i in range(len(self.lista_lbl)):
+        for i in range(len(self.procesos)):
             if self.procesos[i].estado == "Ejecutando":
                 self.lista_lbl[i]['background'] = 'green'
             elif self.procesos[i].estado == "Bloqueado":
@@ -64,28 +58,45 @@ class Sistema:
     def detener_sim(self):
         self.correr_sim = False
 
-    def agregar_proceso_lista(self):
+    def agregar_proceso(self):
         if(self.dr_nv_proc.get().isdigit()):
             self.nv_bloq.sort()
             self.nv_bloq.reverse()
-            self.procesos.append(Proceso(self.quantum, int(self.dr_nv_proc.get()), self.nv_bloq))
+            self.lista_procesos.append(Proceso(self.quantum, int(self.dr_nv_proc.get()), self.nv_bloq))
 
-            i = len(self.procesos)-1
-            pnl = ttk.LabelFrame(self.panel_procesos, text="Proceso"+str(i))
-            lb = ttk.Label(pnl, text='P'+str(i), background="red", foreground='white')
-            lb.grid(row=0, column=0)
-            self.lista_lbl.append(lb)
-            ttk.Button(pnl, text='||', width=3).grid(row=0,column=1)
-            ttk.Button(pnl, text='■', width=3).grid(row=0,column=2)
-            pnl.grid(row=i%2, column=int(i/2))
+            self.agregar_proceso_lista()
 
             self.nv_bloq = []
             self.lb_nv_bloq.set('')
-    
+
+    def agregar_proceso_lista(self):
+        i = len(self.lista_procesos)-1
+        pnl = ttk.LabelFrame(self.panel_procesos, text="Proceso"+str(i))
+        lb = ttk.Label(pnl, text='P'+str(i), background="black", foreground='white')
+        lb.grid(row=0, column=0)
+        self.lista_lbl.append(lb)
+        ttk.Button(pnl, text='||', width=3, command= lambda: self.bloquear_proceso(i)).grid(row=0,column=1)
+        ttk.Button(pnl, text='■', width=3).grid(row=0,column=2)
+        pnl.grid(row=i%2, column=int(i/2))
+
     def agr_nv_bloq(self):
         if (self.in_nv_bloq.get().isdigit() and self.dr_nv_bloq.get().isdigit()):
             self.nv_bloq.append([int(self.in_nv_bloq.get()), int(self.dr_nv_bloq.get())])
             self.lb_nv_bloq.set(self.nv_bloq)
+
+    def lista_predeterminada(self):
+        self.lista_procesos.append(Proceso(0, 6, [[3,2]]))
+        self.agregar_proceso_lista()
+        self.lista_procesos.append(Proceso(1, 8, [[1,3]]))
+        self.agregar_proceso_lista()
+        self.lista_procesos.append(Proceso(2, 7, [[5,1]]))
+        self.agregar_proceso_lista()
+        self.lista_procesos.append(Proceso(4, 3, [[0,0]]))
+        self.agregar_proceso_lista()
+        self.lista_procesos.append(Proceso(6, 9, [[2,4]]))
+        self.agregar_proceso_lista()
+        self.lista_procesos.append(Proceso(6, 2, [[0,0]]))
+        self.agregar_proceso_lista()
 
     def init_ventana_principal(self):
         self.ventana_principal = tk.Tk()
@@ -107,13 +118,14 @@ class Sistema:
         ttk.Button(self.panel_agregar, text="Agregar Bloqueo", command=self.agr_nv_bloq).grid(row=2, column=0)
         ttk.Label(self.panel_agregar, text="ini").grid(row=2, column=1)
         ttk.Label(self.panel_agregar, text="dur").grid(row=2, column=2)
-        ttk.Button(self.panel_agregar, text="Agregar Proceso", command=self.agregar_proceso_lista).grid(row=3, column=0, columnspan=3, sticky='we')
+        ttk.Button(self.panel_agregar, text="Agregar Proceso", command=self.agregar_proceso).grid(row=3, column=0, columnspan=3, sticky='we')
 
         # panel de control de simulacion
-        self.panel_ctrl_sim = ttk.LabelFrame(self.ventana_principal, text="Control de simulación")
-        self.panel_ctrl_sim.grid(row=0, column=1, sticky="e")
+        self.panel_ctrl_sim = ttk.LabelFrame(self.ventana_principal, text="Control")
+        self.panel_ctrl_sim.grid(row=0, column=1, sticky="w")
         ttk.Button(self.panel_ctrl_sim, text="Iniciar", command=self.iniciar_sim).grid(row=0, column=0, sticky='we')
-        ttk.Button(self.panel_ctrl_sim, text="Detener", command=self.detener_sim).grid(row=0, column=1, sticky='we')
+        ttk.Button(self.panel_ctrl_sim, text="Detener", command=self.detener_sim).grid(row=1, column=0, sticky='we')
+        ttk.Button(self.panel_ctrl_sim, text="Predet.", command=self.lista_predeterminada).grid(row=2, column=0, sticky='we')
 
         self.lista_lbl = []
         # panel de lista de procesos
