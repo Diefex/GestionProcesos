a376dbee5be15cd7dac26d7c5e819ec9095972a6
Diefex <diefex57@gmail.com>
2021-10-11 16:37:52 -0500

    Mejora Display 3

--- Display.py
+++ Display.py
@@ -3,7 +3,7 @@ from tkinter import Canvas
 class Display:
     def __init__(self, master, tam_cr=20, y_crr=20, x_crr=35, width=0, height=200):
         self.tam_cr = tam_cr
-        self.y_crr = y_crr
+        self.y_crr = height-y_crr
         self.x_crr = x_crr
 
         self.width = self.x_crr+(self.tam_cr*width)
@@ -15,15 +15,15 @@ class Display:
 
         self.pos = self.x_crr
 
-        self.canvas.create_line(self.x_crr, self.y_crr-1, self.width-self.tam_cr, self.y_crr-1, fill='white')   
+        self.canvas.create_line(self.x_crr, self.y_crr+1, self.width-self.tam_cr, self.y_crr+1, fill='white')   
 
     def pintar_procesos(self, procesos, RR=False):
         self.canvas.create_rectangle(0,0, self.x_crr, self.height, fill='black')
-        self.canvas.create_text(self.pos+7, self.y_crr-10, text=str(int(self.pos/self.tam_cr)), fill="white")
+        self.canvas.create_text(self.pos+7, self.y_crr+10, text=str(int(self.pos/self.tam_cr)), fill="white")
         for i in range(len(procesos)):
-            self.canvas.create_text(self.x_crr-15, self.y_crr+8+(i*self.tam_cr), text="RR" if RR and i==0 else "P"+str(i), fill="white")
-            self.canvas.create_line(self.x_crr-5, self.y_crr+(i*self.tam_cr), self.x_crr-5, self.y_crr+self.tam_cr+(i*self.tam_cr), fill="white")
-            self.canvas.create_line(self.x_crr-8, self.y_crr+self.tam_cr+(i*self.tam_cr), self.x_crr-1, self.y_crr+self.tam_cr+(i*self.tam_cr), fill="white")
+            self.canvas.create_text(self.x_crr-15, self.y_crr-8-(i*self.tam_cr), text="RR" if RR and i==0 else "P"+str(i), fill="white")
+            self.canvas.create_line(self.x_crr-5, self.y_crr-(i*self.tam_cr), self.x_crr-5, self.y_crr-self.tam_cr-(i*self.tam_cr), fill="white")
+            self.canvas.create_line(self.x_crr-8, self.y_crr-self.tam_cr-(i*self.tam_cr), self.x_crr-1, self.y_crr-self.tam_cr-(i*self.tam_cr), fill="white")
 
             status = procesos[i].estado
             if(status == "Ejecutando"):
@@ -38,6 +38,6 @@ class Display:
                 color = "cyan"
 
             x = self.pos
-            y = (i*self.tam_cr)+self.y_crr
-            self.canvas.create_rectangle(x,y,x+self.tam_cr,y+self.tam_cr, fill=color)
+            y = self.y_crr-(i*self.tam_cr)
+            self.canvas.create_rectangle(x,y,x+self.tam_cr,y-self.tam_cr, fill=color)
         self.pos += self.tam_cr
